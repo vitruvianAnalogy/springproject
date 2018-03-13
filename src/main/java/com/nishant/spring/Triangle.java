c3696d27be54946b08932b24f88089e33bfdb808
Nishant Rawat <nishantrawat22@gmail.com>
2018-03-12 23:02:11 -0700

    Constructor injection. Spring converts '20' to 20 if int is expected

--- src/main/java/com/nishant/spring/Triangle.java
+++ src/main/java/com/nishant/spring/Triangle.java
@@ -3,6 +3,16 @@ package com.nishant.spring;
 public class Triangle {
 	
 	private String type;
+	private int height;
+	
+	public Triangle (String type){
+		this.type = type;
+	}
+	
+	public Triangle(String type, int height){
+		this.type = type;
+		this.height = height;
+	}
 	
 	public String getType() {
 		return type;
@@ -13,6 +23,16 @@ public class Triangle {
 	}
 
 	public void draw(){
-		System.out.println("Triangle BAM! " +getType()+" Hell yeah!!!!");
+		System.out.println("Triangle BAM! " +getType()+" Hell yeah!!!! and " + getHeight() + " feet tall");
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+	public void setHeight(int height) {
+		this.height = height;
 	}
 }
