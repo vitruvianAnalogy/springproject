4bdeaa6f801e7f567358477452b0842d18d6994b
Nishant Rawat <nishantrawat22@gmail.com>
2018-03-13 23:12:45 -0700

    Injecting LIST/Collection of beans within a bean

---
+++ src/main/java/com/nishant/spring/TriangleWithListOfPoints.java
@@ -0,0 +1,21 @@
+package com.nishant.spring;
+
+import java.util.List;
+
+public class TriangleWithListOfPoints {
+		private List<Point> listOfPoints;
+
+		public List<Point> getListOfPoints() {
+			return listOfPoints;
+		}
+
+		public void setListOfPoints(List<Point> listOfPoints) {
+			this.listOfPoints = listOfPoints;
+		}
+		
+		public void draw(){
+			for(Point p : listOfPoints){
+				System.out.println("Point = (" + p.getX() + "," + p.getY() + ")");
+			}
+		}
+}
